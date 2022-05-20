ed45cb361a162c198276207028d68c94ad2d3d0f
sosworkuacha <sosworkuacha@gmail.com>
2022-05-20 22:27:22 +0300

    spring initial project

---
+++ tutorapp/src/main/java/com/mobileapp/tutorapp/TutorappApplication.java
@@ -0,0 +1,13 @@
+package com.mobileapp.tutorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorappApplication.class, args);
+	}
+
+}
---
+++ tutorapp/src/test/java/com/mobileapp/tutorapp/TutorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileapp.tutorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
