c0375d8fe96116d9d33288139679cc4b8116eb6b
[genzebalemu] <[genzebalemu46@gmail.com]>
2022-06-05 11:58:34 +0300

    comment page finalization

--- tutorapp/test/widget_test/tutorials/catagories_widget_test.dart
+++ tutorapp/test/widget_test/tutorials/catagories_widget_test.dart
@@ -13,11 +13,10 @@ void main() {
   testWidgets('testing catagories widgets', (WidgetTester tester) async {
     final titleFinder = find.text('Text');
     await tester.pumpWidget(MaterialApp(
-      home: catagories(
-        course: Tutorial(
-            id: 021, code: "456", title: "flutter", description: "flutter"),
-        tutorialRepositoy: TutorialRepositoy(TutorialDataProvider()),
-      ),
+      home: category(
+          // TutorialRepositoy
+          // tutorialRepositoy: TutorialRepositoy(TutorialDataProvider()),
+          ),
     ));
     expect(titleFinder, findsOneWidget);
   });
